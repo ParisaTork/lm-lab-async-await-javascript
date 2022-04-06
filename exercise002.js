5370e7d559f7b3067234ddb2d96a6881d621f9f2
ParisaTork <47482049+ParisaTork@users.noreply.github.com>
2022-04-06 14:25:41 +0100

    Exercise 2 Task 1 done

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,21 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
-};
+// const fetchData = (apiEndPoint) => {
+//   fetch(apiEndPoint)
+//     .then((response) => response.json())
+//     .then((json) => console.log(json))
+//     .catch((error) => console.log(error));
+// };
 
 fetchData(jsonTypicode);
+
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    console.log(json);
+  } catch(error) {
+    console.log(error);
+  }
+}
